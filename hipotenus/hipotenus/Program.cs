36fb76c053dd0e14352d45eed1f5bdbcfcc9d5d6
omer kor <omerkorr@gmail.com>
2020-02-04 12:31:26 +0300

    last

--- hipotenus/hipotenus/Program.cs
+++ hipotenus/hipotenus/Program.cs
@@ -10,6 +10,14 @@ namespace hipotenus
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("1.kenar uzunluğunu giriniz");
+            double kenar1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("2.kenar uzunluğunu giriniz");
+            double kenar2 = Convert.ToDouble(Console.ReadLine());
+            double kareleritoplami = Math.Pow(kenar1, 2) + Math.Pow(kenar2, 2);
+            double hipoLong = Math.Sqrt(kareleritoplami);
+            Console.WriteLine("hipotenüs uzunluğu: {0}", hipoLong);
+            Console.ReadKey();
         }
     }
 }
